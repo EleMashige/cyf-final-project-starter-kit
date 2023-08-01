cc371980a323e326125b7712920aace0dd882e49
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-08-01 16:52:16 +0100

    chang port to 3000

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,29 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Navbar from "./Navbar";
+import StarList from "./StarList";
 import "./Home.css";
 
 
 export function Home() {
 	const client_id = "a823fe614b9796fe502f";
-    const redirect_uri = "http://localhost:3100/api/auth/github";
-    const githubLoginUrl = "https://github.com/login/oauth/authorize";
+    const redirect_uri = "http://localhost:3000/api/auth/github";
+	const githubLoginUrl = "https://github.com/login/oauth/authorize";
     const state = "secret-key";
     const url = `${githubLoginUrl}?client_id=${client_id}&redirect_uri=${redirect_uri}&state=${state}`;
 	return (
 		<main role="main">
 			<div>
-				{/* <img
-					className="logo"
-					data-qa="logo"
-					src={logo}
-					alt="Just the React logo"
-				/>
-				<h1 className="message" data-qa="message">
-					{message}
-	           </h1> */}
-			<Link to={url}>GitHub login:</Link>
+				<Navbar />
+				<Link to={url}>GitHub login:</Link>
+				<StarList />
 			</div>
 		</main>
 	);
-}
-export default Home;
+	}
+
+	export default Home;
+
