fe1561a414c133603ec21fac9a3ef00003de6bf6
Mathias02 <matthiasm2016@gmail.com>
2023-08-07 12:52:13 +0200

    data populated

--- client/src/Components/Trainee Dashboard/TraineeTracker.js
+++ client/src/Components/Trainee Dashboard/TraineeTracker.js
@@ -1,14 +1,17 @@
 import React, {useState, useEffect} from 'react';
 import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
-import './TraineeTracker.css'
-{/*import { resetWarningCache } from 'prop-types';*/}
+import './TraineeTracker.css';
 
 const TraineeTracker = (name) => {
 
+ {/*states */}
+ 
   const [prs, setPrs] = useState(0);
   const [show, setShow] = useState(false);
-  const [codeWarsRank, setCodeWarsRank] = useState(0);
+  
+
+  {/* fetching data from GitHub Api */}
 
   useEffect(() => {
     fetch(`https://api.github.com/search/issues?q=is:pr%20author:${name}%20user:codeyourfuture`)
@@ -22,7 +25,10 @@ const TraineeTracker = (name) => {
     return(
         <div className="traineeD">
             <Navbar />
-            <h1>Hello trainee, below is your tracked total score</h1>           
+            <h1>Hello trainee, below is your tracked total score</h1>  
+
+            {/*table to display data in the browser*/}      
+
             <div className='tablescore'>
               <table>
                 <thead>
