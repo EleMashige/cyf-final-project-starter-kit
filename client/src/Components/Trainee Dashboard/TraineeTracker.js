e8d25c011adc39335646e87e2c38fdbd24dd85e1
EleMashige <elekanyanimashige@yahoo.com>
2023-08-10 19:55:12 +0200

    Created table for trainee tracker

--- client/src/Components/Trainee Dashboard/TraineeTracker.js
+++ client/src/Components/Trainee Dashboard/TraineeTracker.js
@@ -1,15 +1,53 @@
-import React from "react";
-import Navbar from "../Navbar/Navbar";
-import Footer from "../Footer/Footer";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Navbar from '../Navbar/Navbar';
+import Footer from '../Footer/Footer';
+import './TraineeTracker.css'; // Import your custom CSS file for styling
 
-const TraineeTracher = () => {
-    return(
+const TraineeTracker = () => {
+    const [progressData, setProgressData] = useState([]);
+
+    useEffect(() => {
+        axios.get('/api/traineeProgress')
+            .then(response => {
+                setProgressData(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
+
+    return (
         <div>
-        <Navbar />
-            <h1>Hello trainee, below is your tracker</h1>
+            <Navbar />
+            <div className="trainee-tracker-container">
+            <div className="tracker-heading"> <h1>Hello Name, below is your tracker</h1> </div>
+            <table className="tracker-table">
+                <thead>
+                    <tr>
+                        <th>Milestones</th>
+                        <th>Date</th>
+                        <th>Required Pull Requests</th>
+                        <th>Codewars</th>
+                        <th>Cohort</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {progressData.map((entry, index) => (
+                        <tr key={index}>
+                            <td>{entry.milestones}</td>
+                            <td>{new Date(entry.date).toLocaleDateString()}</td>
+                            <td>{entry.required_pull_requests}</td>
+                            <td>{entry.codewars}</td>
+                            <td>{entry.cohort}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+</div>
             <Footer />
         </div>
     );
 };
 
-export default TraineeTracher;
+export default TraineeTracker;
