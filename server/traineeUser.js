821089c2bed8ddc5a23362997e90738296d6c0f3
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-08-14 13:58:28 +0100

    define the endpoint path without the base /api/trainees

--- server/traineeUser.js
+++ server/traineeUser.js
@@ -1,16 +1,25 @@
-const pool = require("./db");
+const express = require("express");
+const router = express.Router();
+const { getTraineeByGithubName, getCohortById } = require("../traineeUser");
 
-async function getTraineeByGithubName(githubName) {
-  const result = await pool.query("SELECT * FROM trainee WHERE github_name = $1", [githubName]);
-  return result.rows[0];
-}
+router.get("/", async (req, res) => {
+    const githubName = req.query.github_name;
 
-async function getCohortById(cohortId) {
-  const result = await pool.query("SELECT * FROM cohorts WHERE id = $1", [cohortId]);
-  return result.rows[0];
-}
+    if(!githubName) {
+        return res.status(400).json({ error: "github_name is required" });
+    }
 
-module.exports = {
-  getTraineeByGithubName,
-  getCohortById,
-};
+    const traineeData = await getTraineeByGithubName(githubName);
+    if(!traineeData) {
+        return res.status(404).json({ error: "No trainee found with provided github_name" });
+    }
+
+    const cohortData = await getCohortById(traineeData.cohort_id);
+    if(!cohortData) {
+        return res.status(404).json({ error: "No cohort found for the trainee" });
+    }
+
+    return res.json(cohortData);
+});
+
+module.exports = router;
