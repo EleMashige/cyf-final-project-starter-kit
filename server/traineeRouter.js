0c0f76abbf8174205c7f55d4b44d13b63775dc20
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-08-14 14:00:39 +0100

    define the endpoint path without the base /api/trainees

---
+++ server/traineeRouter.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+const { getTraineeByGithubName, getCohortById } = require("../traineeUser");
+
+router.get("/", async (req, res) => {
+    const githubName = req.query.github_name;
+
+    if(!githubName) {
+        return res.status(400).json({ error: "github_name is required" });
+    }
+
+    const traineeData = await getTraineeByGithubName(githubName);
+    if(!traineeData) {
+        return res.status(404).json({ error: "No trainee found with provided github_name" });
+    }
+
+    const cohortData = await getCohortById(traineeData.cohort_id);
+    if(!cohortData) {
+        return res.status(404).json({ error: "No cohort found for the trainee" });
+    }
+
+    return res.json(cohortData);
+});
+
+module.exports = router;
