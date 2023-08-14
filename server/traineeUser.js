0168fe2f7a962354d192ea024584fc1c3dd92d81
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-08-14 13:59:27 +0100

    remove the mistake

--- server/traineeUser.js
+++ server/traineeUser.js
@@ -1,25 +1,16 @@
-const express = require("express");
-const router = express.Router();
-const { getTraineeByGithubName, getCohortById } = require("../traineeUser");
+const pool = require("./db");
 
-router.get("/", async (req, res) => {
-    const githubName = req.query.github_name;
+async function getTraineeByGithubName(githubName) {
+  const result = await pool.query("SELECT * FROM trainee WHERE github_name = $1", [githubName]);
+  return result.rows[0];
+}
 
-    if(!githubName) {
-        return res.status(400).json({ error: "github_name is required" });
-    }
+async function getCohortById(cohortId) {
+  const result = await pool.query("SELECT * FROM cohorts WHERE id = $1", [cohortId]);
+  return result.rows[0];
+}
 
-    const traineeData = await getTraineeByGithubName(githubName);
-    if(!traineeData) {
-        return res.status(404).json({ error: "No trainee found with provided github_name" });
-    }
-
-    const cohortData = await getCohortById(traineeData.cohort_id);
-    if(!cohortData) {
-        return res.status(404).json({ error: "No cohort found for the trainee" });
-    }
-
-    return res.json(cohortData);
-});
-
-module.exports = router;
+module.exports = {
+  getTraineeByGithubName,
+  getCohortById,
+};
