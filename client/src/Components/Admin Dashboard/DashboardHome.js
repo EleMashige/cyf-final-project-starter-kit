2a6b748daf98453bbc1e83542b264c4ec4e88123
Tresor Bipa Mabundi <66829445+TresorBipa@users.noreply.github.com>
2023-08-15 19:33:54 +0200

    Giving some space between update module and track trainee progress

--- client/src/Components/Admin Dashboard/DashboardHome.js
+++ client/src/Components/Admin Dashboard/DashboardHome.js
@@ -1,40 +1,38 @@
 
-import React from 'react';
-import Navbar from '../Navbar/Navbar';
-import Footer from '../Footer/Footer';
-import { Link } from 'react-router-dom';
-import './DashboardHome.css';
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
+import { Link } from "react-router-dom";
+import "./DashboardHome.css";
 
 
 const DashboardHome = () => {
   return (
-<div>
-<Navbar />
+		<div>
+			<Navbar />
 
-<div className='adminBtn'>
-      <button className="btnlogin">
+			<div className="adminBtn">
+				<button className="btnlogin">
+					Hi Admin, welcome to your dashboard!
+				</button>
+			</div>
 
-        Hi Admin, welcome to your dashboard!
-      </button>
-</div>
+			{/* Log in buttons */}
 
-{/* Log in buttons */}
-
-<div className='adminbody'>
-      <div className='admindiv'>
-        {/* Use absolute paths starting with a forward slash */}
-        <Link to="/update-modules">
-          <button className='adminbodybtn' >Update Modules</button>
-        </Link>
-        <Link to="/track-trainee-progress">
-          <button>Track Trainee Progress</button>
-
-        </Link>
-      </div>
-  </div>
-<Footer />
-</div>
-  );
+			<div className="adminbody">
+				<div className="admindiv">
+					{/* Use absolute paths starting with a forward slash */}
+					<Link to="/update-modules">
+						<button className="adminbodybtn">Update Modules</button>
+					</Link>
+					<Link to="/track-trainee-progress">
+						<button className="adminbodybtn">Track Trainee Progress</button>
+					</Link>
+				</div>
+			</div>
+			<Footer />
+		</div>
+	);
 };
 
 export default DashboardHome;
