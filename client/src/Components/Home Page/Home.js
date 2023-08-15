ce6a1466b51c13cbb72efc7eed08319b4313deb1
Mathias02 <matthiasm2016@gmail.com>
2023-08-15 14:44:44 +0200

    css file created

--- client/src/Components/Home Page/Home.js
+++ client/src/Components/Home Page/Home.js
@@ -3,57 +3,22 @@ import React from "react";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
 import LoginButtons from "./LoginButtons";
-
-import backgroundImage from "../../../src/assets/citite2.jpeg"; // Adjust the path
+import './Home.css';
+ // Adjust the path
+ 
+import backgroundImage from "../../../src/assets/citite2.jpeg";
 
 const Home = () => {
 
 	return (
-		<div
-			style={{
-				position: "relative",
-				width: "100vw", // Use viewport width to cover the entire page
-				height: "100vh", // Use viewport height to cover the entire page
-				overflow: "hidden", // Prevent content from overflowing
-			}}
-		>
-			<img
-				src={backgroundImage}
-				alt="Background"
-				style={{
-					position: "absolute",
-					top: 0,
-					left: 0,
-					width: "100%",
-					height: "100%",
-					objectFit: "cover",
-					zIndex: -1,
-				}}
-			/>
+		<div className='home_container'>
+			<img  src={backgroundImage}  alt="Background" className='bgi' />
 			<Navbar />
-			<div
-				style={{
-					display: "flex",
-					flexDirection: "column",
-					justifyContent: "center",
-					alignItems: "center",
-					height: "100%",
-				}}
-			>
-				<button
-					style={{
-						width: "700px",
-						padding: "10px",
-						fontSize: "40px",
-						color: "red", // Add the red color
-						zIndex: 1,
-						marginBottom: "20px", // Adjust spacing
-						backgroundColor: "rgba(255, 255, 255, 0.7)", // Add some transparency to the button
-					}}
-				>
+			<div className="container_btn">
+				<button className='welcome_btn'>
 					Welcome to CYF Progress Tracker
 				</button>
-				<LoginButtons />
+			<LoginButtons />
 			</div>
 
 			<Footer />
